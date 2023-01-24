80c2a127ecda5e7d9922f2d9361414ec3a15d638
takahashikanon <s18141099@gmail.com>
2023-01-24 10:34:22 +0900

    GET queryparameter

--- http_server.js
+++ http_server.js
@@ -1,16 +1,26 @@
 const express = require('express')
 const ping = require('./src/main.js')
+const cors = require('cors');
 const app = express()
 const port = 8080
 
-// http://localhost:3000/ でresponce.jsonの中身を返す。
+app.use(cors());
+
 app.get("/", (req, res, next) => {
 
-    console.log(req.method, req.url)
-    res.json(ping.pingForWindows('10.0.1.21', 1))
+    let client = {
+        method: req.method,
+        url: req.url,
+        query: req.query,
+    };
+
+    console.log(client)
+
+    let address = String(req.query.ip);
+
+    res.json(ping.pingForWindows(address, 1))
     res.end();
 
 });
 
-// ポート3000でサーバ起動
 app.listen(port, () => console.log(`click http://localhost:${port}/ !`))
--- src/main.js
+++ src/main.js
@@ -12,39 +12,81 @@ function toString(bytes, originStringCode) {
 // module付けないと参照できない！
 module.exports = {
     pingForWindows: function (addressName = '', count = 1) {
-        const command = `ping -n ${count} ${addressName}`;
-        const data = toString(execSync(command), 'SJIS');
+        try {
+            const command = `ping -n ${count} ${addressName}`;
+            const data = toString(execSync(command), 'SJIS');
 
-        let header = data.split('\r\n')[1];
-        let body = data.split('\r\n')[2];
-        let address = header.split(' ')[0];
-        let bytes = body.split(' ')[3].replace('=', '');
-        let time = body.split(' ')[5].replace('=', '').replace('<', '');
+            let header = data.split('\r\n')[1];
+            let body = data.split('\r\n')[2];
+            let address = header.split(' ')[0];
+            let bytes = body.split(' ')[3].replace('=', '');
+            let time = body.split(' ')[5].replace('=', '').replace('<', '');
+            let date = new Date();
+            let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-        let obj = {
-            address: address,
-            byte: bytes,
-            time: time
-        };
+            let obj = {
+                address: address,
+                byte: bytes,
+                time: time,
+                timestamp: timestamp
+            };
 
-        return obj;
+            return obj;
+        } catch {
+            let date = new Date();
+            let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+            let obj = {
+                address: null,
+                byte: null,
+                time: null,
+                timestamp: timestamp
+            };
+
+            return obj;
+        }
     },
     pingForLinux: function (addressName = '', count = 1) {
-        const command = `ping -c ${count} ${addressName}`;
-        const data = toString(execSync(command), 'UTF-8');
+        try {
+            const command = `ping -c ${count} ${addressName}`;
+            const data = toString(execSync(command), 'UTF-8');
 
-        let header = data.split('\n')[0];
-        let body = data.split('\n')[1];
-        let address = header.split(' ')[1];
-        let bytes = header.split(' ')[3];
-        let time = body.split(' ')[6].split('=')[1];
+            let header = data.split('\n')[0];
+            let body = data.split('\n')[1];
+            let address = header.split(' ')[1];
+            let bytes = header.split(' ')[3];
+            let time = body.split(' ')[6].split('=')[1];
+            let date = new Date();
+            let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
 
-        let obj = {
-            address: address,
-            byte: bytes,
-            time: time
-        };
+            let obj = {
+                address: address,
+                byte: bytes,
+                time: time,
+                timestamp: timestamp
+            };
 
-        return obj;
+            return obj;
+        } catch {
+            let date = new Date();
+            let timestamp = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+
+            let obj = {
+                address: null,
+                byte: null,
+                time: null,
+                timestamp: timestamp
+            };
+
+            return obj;
+        }
     }
 };
+
+
+function windows(addressName = '', count = 1) {
+
+
+}
+
+console.log(windows('10.0.1'));
